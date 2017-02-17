07e97e8c9d78e4dc0cbd0f3d851554cbb59fae32
Konstantin Vekilski <konstantin.vekilski@gmail.com>
2017-02-17 11:46:31 +0200

    Update 1.c

--- 2/OtherStuff/1.c
+++ 2/OtherStuff/1.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 #define SIZE 10
 
-void   inputArray(int*);
-void   printArray(int[]);
-double averageArray(int[]);
-int    minIndex(int[]);
+void   inputArray (int*);
+void   printArray (int[]);
+double averageArray (int[]);
+int    minIndex (int[]);
 
 int main() {
 	int array[SIZE];
-	printf("Array size = %d\n", SIZE);
-	inputArray(array);
-	printArray(array);
-	printf("Average array: %lf\n", averageArray(array));
-	printf("Min index: %d\n", minIndex(array));
+	printf ("Array size = %d\n", SIZE);
+	inputArray (array);
+	printArray (array);
+	printf ("Average array: %lf\n", averageArray(array));
+	printf ("Min index: %d\n", minIndex(array));
 	return 0;
 }
 
-void inputArray(int *arr) {
-	printf("INput array:\n");
+void inputArray (int *arr) {
+	printf ("INput array:\n");
 	int i ;	
-	for(i = 0; i < SIZE; i++) {
-		printf("A[%d]=",i);
-		scanf("%d", arr+i);
+	for (i = 0; i < SIZE; i++) {
+		printf ("A[%d]=",i);
+		scanf ("%d", arr+i);
 	}
 }
 
-void printArray(int arr[]) {
+void printArray (int arr[]) {
 	int i;
-	printf("Print array:\n");
-	for(i = 0; i < SIZE; i++) {
-		printf("A[%d]=%d\n",i, *(arr+i));
+	printf ("Print array:\n");
+	for (i = 0; i < SIZE; i++) {
+		printf ("A[%d]=%d\n",i, *(arr+i));
 	}
 }
 
-double averageArray(int arr[]) {
+double averageArray (int arr[]) {
 	int i, sum = 0;
 	for (i = 0; i < SIZE; i++) {
 		sum += arr[i];
@@ -41,7 +41,7 @@ double averageArray(int arr[]) {
 	return (double) sum / SIZE;
 }
 
-int minIndex(int arr[]) {
+int minIndex (int arr[]) {
 	int i, index = 0;
 	for (i = 0; i < SIZE; i++) {
 		if (arr[i] < arr[index]) {
