4981789384885804173844079a56162b09805176
Konstantin Vekilski <konstantin.vekilski@gmail.com>
2017-02-24 16:19:56 +0200

    Create 1.c

---
+++ 2/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+    int sum = 0, a, b;
+    scanf ("%d %d", &a, &b);
+    if (a > b) {
+        a += b;
+        b = a - b;
+        a -= b;
+    } 
+    for (; ++ a < b;) {
+        sum += a;
+    }
+    printf ("%d\n", sum);
+    return 0;
+}
